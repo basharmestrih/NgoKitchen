03f87a883a1d6d92b8f7667753db802fce47e1a8
basharmestrih <mestbashar@gmail.com>
2025-08-14 17:19:52 +0300

    Delete all files

--- src/app/components/SessionProviderWrapper.js
+++
@@ -1,8 +0,0 @@
-// src/app/components/SessionProviderWrapper.js
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
-export default function SessionProviderWrapper({ children }) {
-  return <SessionProvider>{children}</SessionProvider>;
-}
--- src/middleware.js
+++
@@ -1,6 +0,0 @@
-// middleware.js
-export { default } from "next-auth/middleware";
-
-export const config = {
-  matcher: ["/"], // protect home page
-};
